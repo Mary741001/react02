1baa863b26946bb907814c7e84f060c68bcbd72c
Maria <mary_01_74@htmail.com>
2023-10-17 19:13:07 -0500

    V1.nuevo proyecto de react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Componente from './componente/Componente';
+
+function App() {
+  return (
+    <div>
+      <Componente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componente/Componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Componente = () => {
+  return (
+    <div>
+      <h1>Componente1</h1>
+      como estas
+    </div>
+  )
+}
+
+export default Componente
