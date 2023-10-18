49e8f7d3d871523656a0867f127d396d46278098
Maria <mary_01_74@htmail.com>
2023-10-18 18:42:37 -0500

    v2. Diseño App.js y AppForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Componente from './componente/Componente';
+import AppForm from './componente/AppForm';
+
 
 function App() {
   return (
-    <div>
-      <Componente/>
+    <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
+      <AppForm />
+      <p>No. 1 Juan Manuel     x   A</p>
+      <p>No. 2 Juan Manuel     x   A</p>
     </div>
   );
 }
